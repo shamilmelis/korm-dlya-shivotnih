66a16f57f157f63549b9cfd76f97f30555706b21
shamilmelis <akastramormoley1st@mail.ru>
2024-11-22 11:20:57 +0600

    popup is added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -57,7 +57,7 @@ const Header = ({prodo, mainState, mainSetState}) => {
                             <h1 className={'header_title'}>AnimalFOOD</h1>
                             <button className={'header_burger'} onClick={() => setIsBurger(!isBurger)}>=</button>
                         </div>
-                        {/*<button className={'bucket_btn'} onClick={() => setIsPopup(true)}><i className="fa-solid fa-cart-shopping"></i></button>*/}
+                        <button className={'bucket_btn'} onClick={() => setIsPopup(true)}><i className="fa-solid fa-cart-shopping"></i></button>
                         <div className={'header_searcher'}>
                             <input type="text" placeholder={'Найти товар'} className={'search_input'}/>
                             <button className={'search_button'}>найти</button>
@@ -65,48 +65,48 @@ const Header = ({prodo, mainState, mainSetState}) => {
                     </div>
                 </div>
 
-                {/*<div className={isPopup === false ? 'bucket_popup' : 'bucket_popup Active'}>*/}
-                {/*    <div className={'bucket_popup_box'}>*/}
-                {/*        <div className={'close_popup'}>*/}
-                {/*            <span>Корзина</span>*/}
-                {/*            <button className={'close_popup_btn'} onClick={() => setIsPopup(false)}>x</button>*/}
-                {/*        </div>*/}
-                {/*        <div className="inform_product">*/}
-                {/*            {*/}
-                {/*              isBucket.length === 0 ?*/}
-                {/*                  'добавьте какой-нибудь товар':*/}
-                {/*                  isBucket.map((el, i) => {*/}
-                {/*                      return (*/}
-                {/*                          <div className={'product_card'} key={el.id}>*/}
-                {/*                              <div className={'product_card_inner'}>*/}
-                {/*                                  <img*/}
-                {/*                                      src={el.product_image.length === 0 ? ImageUndefined : el.product_image.map(el => el)}*/}
-                {/*                                      alt="" className={'product_card_img'}/>*/}
-                {/*                                  <div className={'product_card_inner_info'}>*/}
-                {/*                                      <span>{el.product_title.length > 40 ? el.product_title.slice(0, 40) + '...' : el.product_title}</span>*/}
-                {/*                                      <span>артикул: 171120240502</span>*/}
-                {/*                                  </div>*/}
-                {/*                              </div>*/}
-                {/*                              <div className={'product_card_count_inner'}>*/}
-                {/*                                  <button className={'minus_toCount_btn'} onClick={() => minusItem(el, el.product_id)}>-</button>*/}
-                {/*                                  <span className={'get_count_value'}>{el.count}</span>*/}
-                {/*                                  <button className={'plus_toCount_btn'} onClick={() => plusItem(el, el.product_id)}>+</button>*/}
-                {/*                              </div>*/}
-                {/*                              <div className={'product_card_inner_price'}>*/}
-                {/*                                  <span className={'product_card_price'}>{el.product_price}</span>*/}
-                {/*                                  <button className={'delete_card_button'} onClick={() => deleteProduct(i)}>убрать</button>*/}
-                {/*                              </div>*/}
-                {/*                          </div>*/}
-                {/*                      )*/}
-                {/*                  })*/}
-                {/*            }*/}
+                <div className={isPopup === false ? 'bucket_popup' : 'bucket_popup Active'}>
+                    <div className={'bucket_popup_box'}>
+                        <div className={'close_popup'}>
+                            <span>Корзина</span>
+                            <button className={'close_popup_btn'} onClick={() => setIsPopup(false)}>x</button>
+                        </div>
+                        <div className="inform_product">
+                            {
+                              isBucket.length === 0 ?
+                                  'добавьте какой-нибудь товар':
+                                  isBucket.map((el, i) => {
+                                      return (
+                                          <div className={'product_card'} key={el.id}>
+                                              <div className={'product_card_inner'}>
+                                                  <img
+                                                      src={el.product_image.length === 0 ? ImageUndefined : el.product_image.map(el => el)}
+                                                      alt="" className={'product_card_img'}/>
+                                                  <div className={'product_card_inner_info'}>
+                                                      <span>{el.product_title.length > 40 ? el.product_title.slice(0, 40) + '...' : el.product_title}</span>
+                                                      <span>артикул: 171120240502</span>
+                                                  </div>
+                                              </div>
+                                              <div className={'product_card_count_inner'}>
+                                                  <button className={'minus_toCount_btn'} onClick={() => minusItem(el, el.product_id)}>-</button>
+                                                  <span className={'get_count_value'}>{el.count}</span>
+                                                  <button className={'plus_toCount_btn'} onClick={() => plusItem(el, el.product_id)}>+</button>
+                                              </div>
+                                              <div className={'product_card_inner_price'}>
+                                                  <span className={'product_card_price'}>{el.product_price}</span>
+                                                  <button className={'delete_card_button'} onClick={() => deleteProduct(i)}>убрать</button>
+                                              </div>
+                                          </div>
+                                      )
+                                  })
+                            }
 
-                {/*        </div>*/}
-                {/*        <div className={'confirm_product'}>*/}
-                {/*            <Link to={'/confirm-order'} bucket={isBucket} className={'confirm_order_btn'}>Оформить</Link>*/}
-                {/*        </div>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
+                        </div>
+                        <div className={'confirm_product'}>
+                            <Link to={'/confirm-order'} bucket={isBucket} className={'confirm_order_btn'}>Оформить</Link>
+                        </div>
+                    </div>
+                </div>
             </header>
         </div>
     )
